54f415183f79d1dc9efa931f649b2779804a02b8
unknown <felipevieira962@hotmail.com>
2022-04-07 23:47:02 -0300

    feat: firts changes and configs after creating project

---
+++ web/src/Components/Header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <S.MainDiv>
+      <T.MainTitle>CodeLeap Network</T.MainTitle>
+
+      <I.LoginButton>sing in</I.LoginButton>
+    </S.MainDiv>
+  );
+}
